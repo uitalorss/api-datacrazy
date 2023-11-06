2f77fab925bee331f0482f252815ff42e9c495d9
Uítalo <uitalo.urss@gmail.com>
2023-11-06 18:35:48 -0300

    ajuste nas funções para procurar por nome e listagem ordenada

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,21 +1,22 @@
 import { User } from "../entities/User";
 import { AppDataSource } from "../data-source";
+import { Like } from "typeorm";
 
 export const userRepository = AppDataSource.getRepository(User).extend({
   findByName(name: string) {
     const user = this.findOne({
       where: {
-        name,
+        name: Like(`%${name}%`),
       },
     });
     return user;
   },
   getYoungerToOlder() {
-    const users = this.find({ order: { created_at: "ASC" } });
+    const users = this.find({ order: { created_at: "DESC" } });
     return users;
   },
   getOlderToYounger() {
-    const users = this.find({ order: { created_at: "DESC" } });
+    const users = this.find({ order: { created_at: "ASC" } });
     return users;
   },
 });
